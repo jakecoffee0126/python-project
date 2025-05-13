f53888738ed0db65f3bd697ee5a51bbc1015ad46
jakecoffee <jakecoffee.huang@gmail.com>
2025-05-13 12:36:25 -0500

    madlibs

---
+++ madlibs/main.py
@@ -0,0 +1,12 @@
+# string concatenation
+
+adj = input("Adjectives: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("famous person: ")
+
+madlib = f'Computer programming is so {adj}! It makes me so excited all the time because \n I live to {verb1}. Stay hydrated and {verb2} like you are {famous_person}'
+
+print(madlib)
+
+# testing
