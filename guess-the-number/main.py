14dafb8f0acf208f4424725c6ad60da70788f6da
jakecoffee <jakecoffee.huang@gmail.com>
2025-05-13 12:42:07 -0500

    added guess-the-number code

---
+++ guess-the-number/main.py
@@ -0,0 +1,24 @@
+# guess the number
+
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+
+    guess = 0
+    while guess != random_number:
+
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        print(f'Random number is: {random_number}')
+        print(f'Guess number is: {guess}')
+
+        if guess < random_number:
+            print("sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high.")
+
+    print(f"Bingo!!! The random number is {random_number}")
+
+
+guess(10)
