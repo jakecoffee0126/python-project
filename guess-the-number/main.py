c0572b959ef7fdfc795f4135bce716e4e3116ee1
jakecoffee <jakecoffee.huang@gmail.com>
2025-05-13 15:04:07 -0500

    computer guess our number

--- guess-the-number/main.py
+++ guess-the-number/main.py
@@ -1,24 +1,28 @@
-# guess the number
+# guess the number (computer guess the user number)
 
 import random
 
 
-def guess(x):
-    random_number = random.randint(1, x)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != "c":
 
-    guess = 0
-    while guess != random_number:
+        # if low and high are equal, it will throw out an error
+        # the reason we put this if condition here and not in the while loop,
+        # because we want the user to say if computer choice correctly or not
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could high too, cuz low = high
+        feedback = input(
+            f'{guess} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+    print("Bingo, computer guessed your number, {guess}, correctly!!")
 
-        guess = int(input(f'Guess a number between 1 and {x}: '))
-        print(f'Random number is: {random_number}')
-        print(f'Guess number is: {guess}')
 
-        if guess < random_number:
-            print("sorry, guess again. Too low.")
-        elif guess > random_number:
-            print("Sorry, guess again. Too high.")
-
-    print(f"Bingo!!! The random number is {random_number}")
-
-
-guess(10)
+computer_guess(10)
